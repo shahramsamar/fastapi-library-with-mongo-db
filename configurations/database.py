472cc7af4ef82eb1fb464d48244e1fd440f58a8f
shahram <shahramsamar2010@gmail.com>
2025-08-29 16:49:04 +0400

    introduce database

---
+++ configurations/database.py
@@ -0,0 +1,22 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+
+uri = "mongodb://localhost:27017/"
+
+# create a new client and connect to the server
+clinet = MongoClient(uri,server_api=ServerApi("1"))
+
+
+# send a ping to confrim a successful connection
+try:
+    clinet.admin.command("ping")
+    print("Pinged your deployment.You successfilly connected to MongoDb!!!!!")
+except Exception as e:
+    print(e)    
+
+
+db = clinet.library
+colletion_name = db["books"]
+    
