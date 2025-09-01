6b7ade17e13d9dbff172a2da745ac71902dbb3e7
shahram <shahramsamar2010@gmail.com>
2025-09-01 10:52:29 +0330

    search with title

--- main.py
+++ main.py
@@ -19,3 +19,11 @@ async def add_book(book:Book):
     books_collection.insert_one(book_single)
     return {"message":"Book added successfully"}
 
+
+@app.get("/book/{title}")
+def get_book_by_title(title:str):
+    book = books_collection.find_one({"title":title},{"_id":0})
+    if book:
+        return {"book":book}
+    return {"message":"Book not found"}
+
