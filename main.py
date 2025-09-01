48178d623ce4ad988853bebf92f9e3b69bae7968
shahram <shahramsamar2010@gmail.com>
2025-09-01 13:02:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 from fastapi import FastAPI
-from models.model import Book
-from configurations.database import books_collection
+from routers.router import MyRouter
 
-app = FastAPI(debug=True)
-
-
-
-@app.get("/")
-async def book_list():
-    books = list(books_collection.find({},{"_id":0}))
-    return {"books":books}
-    
+app = FastAPI()
+app.include_router(MyRouter)
 
 
-@app.post("/")
-async def add_book(book:Book):
-    book_single = book.dict()
-    books_collection.insert_one(book_single)
-    return {"message":"Book added successfully"}
-
-
-@app.get("/book/{title}")
-def get_book_by_title(title:str):
-    book = books_collection.find_one({"title":title},{"_id":0})
-    if book:
-        return {"book":book}
-    return {"message":"Book not found"}
 
